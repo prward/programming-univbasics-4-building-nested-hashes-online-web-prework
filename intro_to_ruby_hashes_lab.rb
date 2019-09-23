ab917293d520beaadcc767c686ba699126d143a8
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-23 15:35:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,8 @@ def monopoly_with_third_tier
   monopoly = {
 	  :railroads =>
 	  {:pieces => 4, :rent_in_dollars => {
-	    :one_piece_owned =
+	    :one_piece_owned = 25
+	    :
 	  }
 	  }
   }
